d20057cb7ed21fd362f0d635dc5b8220579a50a3
Nishant Dalal <nishantdalal15@gmail.com>
2026-03-26 18:31:52 +0530

    feat: Add Selection sort.

--- src/main/java/com/algorithms/App.java
+++ src/main/java/com/algorithms/App.java
@@ -1,13 +1,28 @@
 package com.algorithms;
 
+import com.algorithms.sorting.Selection;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Selection selection = new Selection();
+        int[] list = { 100, 99, 98, 97, 96 };
+
+        selection.sort(list);
+
+        for (int i = 0; i < list.length; i++) {
+            if (i == 0) {
+                System.out.print("{ ");
+            }
+            System.out.print(list[i]);
+            if (i != list.length - 1) {
+                System.out.print(", ");
+            } else {
+                System.out.print(" }\n");
+            }
+        }
     }
 }
