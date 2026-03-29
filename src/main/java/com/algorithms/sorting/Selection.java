aec075c6995d97b249040a5dbfc5b59227524086
Nishant Dalal <nishantdalal15@gmail.com>
2026-03-29 20:56:49 +0530

    feat: Update Selection sort.

--- src/main/java/com/algorithms/sorting/Selection.java
+++ src/main/java/com/algorithms/sorting/Selection.java
@@ -8,8 +8,12 @@ public class Selection {
      * @param arr
      */
     public void sort(int[] arr) {
-        if(arr == null) {
+        if (arr == null) {
             throw new IllegalArgumentException("Array cannot be null");
+        }
+
+        if (arr.length <= 1) {
+            return;
         }
 
         for (int i = 0; i < arr.length - 1; i++) {
